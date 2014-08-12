c13ec6dff89bdc0089695b4c9f97c78ce8cc683b
Ali Servet Donmez <asd@pittle.org>
2014-08-12 19:35:57 +0300

    Read summary as plaintext field
    --
    Close #3

--- src/parsers/feed_parser.py
+++ src/parsers/feed_parser.py
@@ -3,6 +3,7 @@
 
 import feedparser
 import datetime
+from bs4 import BeautifulSoup
 
 import models
 
@@ -25,15 +26,20 @@ class FeedParser(object):
             entries.append(entry)
         return entries
 
-    @staticmethod
-    def _new_entry(parsed):
+    def _new_entry(self, parsed):
         return models.Entry(
             content      = parsed.content[0].value,
             id           = parsed.id,
             title        = parsed.title,
-            summary      = parsed.summary,
+            summary      = self._summary_as_plaintext(parsed.summary),
             tags         = [x.term for x in parsed.tags],
             # 9-valued tuple: ( year, month, day, hour, minute, second, day of week, day of year, dst-flag )
             # (Ref: http://stackoverflow.com/questions/686717/python-convert-9-tuple-utc-date-to-mysql-datetime-format)
             published_at = datetime.datetime(*(parsed.published_parsed[:6]))
         )
+
+    @staticmethod
+    def _summary_as_plaintext(text):
+        return BeautifulSoup(text, "lxml")\
+            .get_text(strip=True)\
+            .replace(" [...]", "...")
